2bba03f000790ed1ed744be5d95783ca63793246
Duarte Cardoso <caldasdcardoso@gmail.com>
2025-10-11 10:09:38 +0100

    Adding support for local LLMs

--- explainer/service/config.py
+++ explainer/service/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from langchain_ollama import ChatOllama
 from langchain.chat_models import init_chat_model
 
 
@@ -9,6 +10,9 @@ def get_chat_model(model_name: str = "openai:gpt-4.1-mini"):
     api_key = os.getenv("OPENAI_API_KEY")
 
     if not api_key:
-        raise EnvironmentError("API_KEY environment variable not set.")
+        return ChatOllama(
+            model="qwen3:8b",
+            base_url="http://localhost:11434"
+        )
 
     return init_chat_model(model=model_name, api_key=api_key)
